3a05b1a61fcf2001eca9ce4b932fbc703b1d7876
restalize <vila.remedi@gmail.com>
2017-09-22 23:56:52 +0300

    removed cocoen

--- js/rest_script.js
+++ js/rest_script.js
@@ -5,28 +5,23 @@ var $fabars = $('.fa-bars');
 var $fagear = $('.fa-gear');
 var $facode = $('.fa-code');
 var $faareachart = $('.fa-area-chart');
-var $titleofservice =$(".title-of-service")
+var $titleofservice = $(".title-of-service")
 
-$famagic&$fabars&$fagear&$facode&$faareachart&$titleofservice.waypoint(function () {
-        $famagic.addClass("animated fadeInUp");
-        $fabars.addClass("animated flipInX");
-        $fagear.addClass("animated rotateIn");
-        $facode.addClass("animated flipInY");
-        $faareachart.addClass("animated fadeInUp");
-    
+$famagic & $fabars & $fagear & $facode & $faareachart & $titleofservice.waypoint(function () {
+    $famagic.addClass("animated fadeInUp");
+    $fabars.addClass("animated flipInX");
+    $fagear.addClass("animated rotateIn");
+    $facode.addClass("animated flipInY");
+    $faareachart.addClass("animated fadeInUp");
+
     $titleofservice.addClass("animated fadeInDown")
 
-    }
+  }
 
-    , {
-        offset: '90%'
-    });
-/*
-$(window).onload(function() {
-	$(".loader").fadeOut("slow");
-})
-*/
+  , {
+    offset: '90%'
+  });
 
-$(window).on('load', function (){
-   $(".loader").removeClass("loader");
-}); 
+$(window).on('load', function () {
+  $(".loader").removeClass("loader");
+});
